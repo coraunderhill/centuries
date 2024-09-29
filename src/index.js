5805ae23e5dcd3174062a7c443a36985490f87eb
Cora Underhill <coraunderhill@gmail.com>
2024-09-28 18:19:30 -0700

    Renamed exported module to `centuries` and added separate conversion functions

--- src/index.js
+++ src/index.js
@@ -1,18 +1,35 @@
-/**
- * Converts
- * @param {(number|string)} date
- * @returns
- */
-const convert = date => {
+const convertNum = input => {
 
+  return input;
+
+}
+
+const convertStr = input => {
+
+  return input;
+
+}
+
+/**
+ * Outputs the century of a number year or date string
+ * @param {(number|string)} date A number or date string
+ * @returns {boolean} Currently always returns true. Will change this later
+ */
+const centuries = date => {
+
+  /**
+   * `typeof` passed input—WORM
+   */
   const type = typeof date;
 
+  // Check that the type is explicitly a number or a string before continuing
   if (type !== 'number' && type !== 'string') {
-    throw new Error(`${type} is not a number or string.`)
+    throw new Error(`Input must be a number or string: received ${type}`)
   }
 
-  return true;
+  if (type === 'number') { return convertNum(date); }
+  else { return convertStr(date); }
 
 };
 
-module.exports = convert;
+module.exports = centuries;
