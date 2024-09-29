c6f5d97f1031be13da1117dd620cebcee04267cb
Cora Underhill <coraunderhill@gmail.com>
2024-09-29 13:45:12 -0700

    Added tests for date strings

--- src/index.test.js
+++ src/index.test.js
@@ -2,14 +2,26 @@ const convert = require('./index');
 
 const now = new Date();
 
-test("Today as Date object should result in 21", () => {
+test("Today as Date object should result in 21 as a number", () => {
   expect(convert(now)).toBe(21);
 });
 
-test("-2000 should result in -21", () => {
+test("-2000 as a number should result in -21 as a number", () => {
   expect(convert(-2000)).toBe(-21);
 });
 
-test("2024 should result in 21", () => {
+test("2024 as a number should result in 21 as a number", () => {
   expect(convert(2024)).toBe(21);
+});
+
+test("2024 as a string should result in 21 as a number", () => {
+  expect(convert('2024')).toBe(21);
+});
+
+test("1999-12-31 as a string should result in 20 as a number", () => {
+  expect(convert('1999-12-31')).toBe(20);
+});
+
+test("2000-02-32 as a string should throw an error", () => {
+  expect(() => { convert('2000-02-32'); }).toThrow(Error);
 });
